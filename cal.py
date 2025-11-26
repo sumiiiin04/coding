d892c10bf2dad54f9a92c83f185d07293b389c9c
sumiiiin04 <20235012@s.scnu.ac.kr>
2025-11-26 23:29:08 +0900

    cal.py

---
+++ cal.py
@@ -0,0 +1,11 @@
+a = int(input('첫 번째 정수 입력: '))
+b = int(input('두 번째 정수 입력: '))
+sum = a + b
+min = a - b
+mul = a * b
+div = a / b
+
+print(a, '+', b, '=', sum)
+print(a, '-', b, '=', min)
+print(a, '*', b, '=', mul)
+print(a, '/', b, '=', div)
