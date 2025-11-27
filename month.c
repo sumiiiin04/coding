3ca163662d967b2aadf6712c44ff6229e842dc72
sumiiiin04 <20235012@s.scnu.ac.kr>
2025-11-27 17:53:06 +0900

    month.c

---
+++ month.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int month, days;
+
+    printf("달을 입력하시오");
+    scanf("%d", &month);
+
+    switch(month) {
+    case 2:
+        days = 28;
+        break;
+        
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        days = 30;
+        break;
+
+    default:
+        days = 31;
+        break;
+     }
+
+     printf("%d월의 일수는 %d일 입니다", month, days);
+     return 0;
+}
